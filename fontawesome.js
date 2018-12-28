2eda3d73a6d368cc27187154077568424d13de04
Bryan Kok <bryan.wyern1@gmail.com>
2018-12-28 22:12:33 +0800

    Add FA 5 prefixes to hexo extended tags

--- fontawesome.js
+++ fontawesome.js
@@ -1,3 +1,19 @@
 hexo.extend.tag.register('fa', function(args) {
     return '<i class="fa ' + args.map(function(arg) { return 'fa-' + arg; }).join(' ') + '"></i>';
 });
+
+hexo.extend.tag.register('fas', function(args) {
+    return '<i class="fas ' + args.map(function(arg) { return 'fa-' + arg; }).join(' ') + '"></i>';
+});
+
+hexo.extend.tag.register('fab', function(args) {
+    return '<i class="fab ' + args.map(function(arg) { return 'fa-' + arg; }).join(' ') + '"></i>';
+});
+
+hexo.extend.tag.register('far', function(args) {
+    return '<i class="far ' + args.map(function(arg) { return 'fa-' + arg; }).join(' ') + '"></i>';
+});
+
+hexo.extend.tag.register('fal', function(args) {
+    return '<i class="fal ' + args.map(function(arg) { return 'fa-' + arg; }).join(' ') + '"></i>';
+});
